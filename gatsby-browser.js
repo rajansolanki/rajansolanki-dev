379098ea4656d1fcd08f8b57201d504970d137ae
Raj <rajansolanki@users.noreply.github.com>
2020-06-30 22:57:33 +0100

    chore: add external error reporting

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,3 +3,11 @@ import './src/styles/global.css';
 
 import 'zone.js/dist/zone';
 import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter';
+import { init } from '@sentry/react';
+
+init({
+  dsn:
+    'https://09cb08fa63a04312aa385f56b8d71b44@o353586.ingest.sentry.io/5304025',
+  environment: process.env.NODE_ENV,
+  enabled: process.env.NODE_ENV !== 'development',
+});
