eb9ca756ce28b03081df7820d45e0ed839252ee6
Raj <rajansolanki@users.noreply.github.com>
2020-10-07 18:13:24 +0100

    feat: add page route animation

---
+++ src/components/root.spec.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import { Root } from './root';
+
+beforeEach(jest.clearAllMocks);
+afterEach(expect.hasAssertions);
+
+describe('`Root`', () => {
+  beforeEach(setupTest);
+
+  describe('Template', () => {
+    it('should render children', () => {
+      expect(Page.children).toBeTruthy();
+    });
+  });
+});
+
+class Page {
+  static get children(): HTMLElement {
+    return screen.getByText('Content');
+  }
+}
+
+function setupTest(): void {
+  render(
+    <Root>
+      <div>Content</div>
+    </Root>
+  );
+}
