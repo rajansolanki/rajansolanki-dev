3661686f097955c6eeca2a7bc5810f224e31e93f
Raj <rajansolanki@users.noreply.github.com>
2020-10-07 23:01:49 +0100

    feat: add header animation

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -1,14 +1,25 @@
 import React, { FC } from 'react';
 
+import { animated, useTrail } from 'react-spring';
+
 import { Header as HeaderStyled } from './header.styles';
 
-const Header: FC = () => (
-  <HeaderStyled>
-    <div>
-      <h1>Raj</h1>
-      <h2>Designer &amp; Developer</h2>
-    </div>
-  </HeaderStyled>
-);
+const Header: FC = () => {
+  const [h1Styles, h2Styles] = useTrail(2, {
+    from: { height: '0px', transform: 'translateY(0%)' },
+    height: '100%',
+    transform: 'translateY(-50%)',
+    config: { tension: 350 },
+  });
+
+  return (
+    <HeaderStyled>
+      <div>
+        <animated.h1 style={h1Styles}>Raj</animated.h1>
+        <animated.h2 style={h2Styles}>Designer &amp; Developer</animated.h2>
+      </div>
+    </HeaderStyled>
+  );
+};
 
 export { Header };
